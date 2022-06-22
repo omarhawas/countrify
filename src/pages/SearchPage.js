08f5389c0d0262169f5fe0be3af581c01f39770c
Omar Hawas <omarhawas@Omars-MacBook-Air.local>
2022-06-21 21:07:15 -0400

    first commit

---
+++ src/pages/SearchPage.js
@@ -0,0 +1,3 @@
+// this is where we call the API to get the countries on first render.
+// stores the API data in the state.
+// Calls the results component passing in the filtered countries from the search term.
