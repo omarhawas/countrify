8c6dfabbe379d631450534e4d77701f5001d7f8d
Omar Hawas <omarhawas94@gmail.com>
2022-06-22 15:49:12 -0400

    added input and handled change to input state.

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,10 +1,24 @@
-// this is where we call the API to get the countries on first render.
-// stores the API data in the state.
-// Calls the results component passing in the filtered countries from the search term.
 import React from "react";
+import { useState } from "react";
 
 const SearchPage = () => {
-  return <div>Search Page</div>;
+  const [value, setValue] = useState("");
+
+  return (
+    <div>
+      <h1>Search Page</h1>
+      <input
+        type="text"
+        value={value}
+        onChange={(event) => setValue(event.target.value)}
+      ></input>
+      <button>Search</button>
+    </div>
+  );
 };
 
 export default SearchPage;
+
+// this is where we call the API to get the countries on first render.
+// stores the API data in the state.
+// Calls the results component passing in the filtered countries from the search term.
